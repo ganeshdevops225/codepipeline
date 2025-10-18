242c094e34b3a014ce72643d563f995418a19b17
ganeshdevops225 <ganeshdevops225@gmai.com>
2025-10-18 19:04:05 +0530

    code updated

--- main.tf
+++ main.tf
@@ -8,12 +8,20 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "demo-test-codepipeline"
+    key    = "path/to/terraform.tfstate"
+  }
+}
+
 
 resource "aws_instance" "web" {
   ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
-  instance_type = "foo"
+  instance_type = "t2.micro"
   tags = {
     Name = "TerraformServer"
   }
