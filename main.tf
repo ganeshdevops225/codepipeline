445d43bc6236a30189441aa2d74ed9fced076808
ganeshdevops225 <ganeshdevops225@gmai.com>
2025-10-18 18:52:03 +0530

    1st file

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  instance_type = "foo"
+  tags = {
+    Name = "TerraformServer"
+  }
+}
+
